29ba956754765736b480778994f12a3371dc8fee
oereo <dlstpgns0406@gmail.com>
2020-04-11 04:47:34 +0900

    CRUD test

---
+++ blog/urls.py
@@ -0,0 +1,30 @@
+"""blog URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+import blogpost.views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', blogpost.views.home, name="home"),
+    path('blog/<int:blog_id>', blogpost.views.detail, name="detail"),
+    path('blog/new', blogpost.views.new, name="new"),
+    path('blog/create', blogpost.views.create, name="create"),
+    path('blog/delete/<int:blog_id>', blogpost.views.delete, name = "delete"),
+    path('blog/edit/<int:blog_id>', blogpost.views.edit, name = "edit"),
+    path('blog/update/<int:blog_id>', blogpost.views.update, name="update"),
+
+]
---
+++ blogpost/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Blog
+
+# Create your views here.
+
+def home(request):
+    blogs = Blog.objects #쿼리셋 개념 알아야됨!! 꼭꼭
+    return render(request, 'home.html', {'blogs' : blogs})
+
+def detail(request, blog_id):
+    details = get_object_or_404(Blog, pk = blog_id)
+    return render(request, 'detail.html', {'details' : details})
+
+def new(request):
+    return render(request, 'new.html')
+
+def create(request):
+    blog = Blog()
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save() #쿼리셋 메소드
+    return redirect('/blog/'+str(blog.id)) # redirect와 render의 차이점 알려주기!!
+
+def edit(request, blog_id):
+    blog = get_object_or_404(Blog, pk = blog_id)
+    return render(request, 'edit.html', {'blog' : blog})
+
+def delete(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    #blog = Blog.objects.filter(id = blog_id)
+    blog.delete()
+    return redirect('home')
+
+def update(request, blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    blog.title = request.GET['title']
+    blog.body = request.GET['body']
+    blog.pub_date = timezone.datetime.now()
+    blog.save() #쿼리셋 메소드
+    return redirect('/blog/'+str(blog.id))
